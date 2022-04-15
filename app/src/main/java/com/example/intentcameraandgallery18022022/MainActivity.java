15a2a1ba13f1f2b2884844b9891800f7a9f0c05c
Phat <phatdroid94@gmail.com>
2022-04-15 19:25:29 +0700

    permission

--- app/src/main/java/com/example/intentcameraandgallery18022022/MainActivity.java
+++ app/src/main/java/com/example/intentcameraandgallery18022022/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.intentcameraandgallery18022022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.intentcameraandgallery18022022.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mBinding;
+    int REQUEST_CODE_CAMERA = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CODE_CAMERA
+                    );
+                }
+
+            }
+        });
     }
 }
